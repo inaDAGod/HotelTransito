7fd535445d5aaa3e9b42d6c6e27d32e9a5934a00
DAGod <90727076+inaDAGod@users.noreply.github.com>
2025-05-06 21:15:27 -0400

    Update UserService

--- src/main/java/com/example/hoteltransito/service/UserService.java
+++ src/main/java/com/example/hoteltransito/service/UserService.java
@@ -4,7 +4,6 @@ import com.example.hoteltransito.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.Optional;
 
